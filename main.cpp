2138421be9918ac49616c18f6bb6569a2c950f0d
German Douce <germandouce@gmail.com>
2021-10-30 21:03:38 -0300

    agrego main

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "menu.h"
+#include "caracteristicas_edificios.h"
+#include "mapa.h"
+#include "edificio.h"
+
+using namespace std;
+
+
+int main(){
+
+    Mapa * mapa = new Mapa;
+    mapa->procesar_archivo_ubicaciones();
+
+
+    mostrar_menu();
+    int opcion = elegir_opcion();
+
+    while ( opcion != GUARDAR_SALIR ){
+
+        procesar_opcion(opcion, mapa);
+        mostrar_menu();
+        opcion = elegir_opcion();
+    }
+
+    delete mapa;
+
+    return 0;
+}
